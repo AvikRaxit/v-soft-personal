9557b2a29c7badac8b4f519c89b4503c3f3b01fd
Avik Rakshit <39942608+avikrakshit00@users.noreply.github.com>
2022-12-07 16:21:10 +0530

    nav bar changes

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -148,10 +148,6 @@
           </a>
           <div class="dropdown-menu dropdown-menu-end">
             <a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
-            <a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="pie-chart"></i> Analytics</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="index.html"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
-            <a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i> Help Center</a>
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="log-in"></i> <span class="align-middle">Log out</a>
           </div>
