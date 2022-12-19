ae9b09fca53c7daf6c14525fc96ad6b3cfd68426
Avik Rakshit <39942608+avikrakshit00@users.noreply.github.com>
2022-12-19 17:27:32 +0530

    changes

--- app/Http/Controllers/Admin/AdminContrller.php
+++ app/Http/Controllers/Admin/AdminContrller.php
@@ -38,6 +38,9 @@ class AdminContrller extends Controller
                     return redirect()->route('dashboard');
                 }
             }
+            elseif(empty(Authentication::count())) {
+                return back();
+            }
             else {
                 return back();
             }
