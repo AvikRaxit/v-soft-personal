593645fa1dcee5b082d0a1c8052b5ffbf3181859
Avik Rakshit <39942608+avikrakshit00@users.noreply.github.com>
2022-12-22 15:46:06 +0530

    changes and new rar file

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -148,9 +148,9 @@
           </a>
           <div class="dropdown-menu dropdown-menu-end">
             <a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
-            {{-- <div class="dropdown-divider"></div> --}}
-            <a class="dropdown-item" href="{{route('clearAll')}}"><i class="align-middle me-2" data-feather="trash"></i> <span class="align-middle">Clear Cache</a>
-            {{-- <div class="dropdown-divider"></div> --}}
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="{{route('clearAll')}}"><i class="align-middle me-2" data-feather="trash"></i><span class="align-middle">Clear Cache</a>
+            <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="{{route('logout')}}"><i class="align-middle me-1" data-feather="log-in"></i> <span class="align-middle">Log out</a>
           </div>
         </li>
